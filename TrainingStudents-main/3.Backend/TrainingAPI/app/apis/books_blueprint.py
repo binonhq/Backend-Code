b3b5267430a68f72507adb7f8cc93fa4d94c8c28
binonhq <nhquan.239@gmail.com>
2022-10-08 23:12:07 +0700

    .

--- TrainingStudents-main/3.Backend/TrainingAPI/app/apis/books_blueprint.py
+++ TrainingStudents-main/3.Backend/TrainingAPI/app/apis/books_blueprint.py
@@ -1,37 +1,32 @@
-from email.policy import default
-from sre_constants import SUCCESS
 import uuid
-
-from sanic import Blueprint
-from sanic.response import json
 
 from app.constants.cache_constants import CacheConstants
 from app.databases.mongodb import MongoDB
 from app.databases.redis_cached import get_cache, set_cache
 from app.decorators.auth import protected
 from app.decorators.json_validator import validate_with_jsonschema
-from app.hooks.error import ApiBadRequest, ApiForbidden, ApiInternalError, ApiNotFound, ApiUnauthorized
-from app.models.book import update_book_json_schema, create_book_json_schema, Book
-from app.models.user import User
+from app.hooks.error import ApiBadRequest, ApiForbidden, ApiNotFound
 from app.hooks.error import ApiInternalError
-
+from app.models.book import update_book_json_schema, create_book_json_schema, Book
+from sanic import Blueprint
+from sanic.response import json
 
 books_bp = Blueprint('books_blueprint', url_prefix='/books')
 users_bp = Blueprint('books_blueprint', url_prefix='/users')
 
 _db = MongoDB()
 
+
 # r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
-@books_bp.route('/', methods = {'GET'})
+@books_bp.route('/', methods={'GET'})
 async def get_all_book(request):
     # TODO: use cache to optimize api
-     
+
     async with request.app.ctx.redis as r:
         books = await get_cache(r, CacheConstants.all_books)
         if books is None:
-            filter = {}
             book_objs = _db.get_all_books()
             books = [book.to_dict() for book in book_objs]
             await set_cache(r, CacheConstants.all_books, books)
@@ -42,17 +37,17 @@ async def get_all_book(request):
         'n_books': number_of_books,
         'books': books
     })
-    
-@books_bp.route('/<book_id>', methods = {'GET'})
+
+
+@books_bp.route('/<book_id>', methods={'GET'})
 async def get_book(request, book_id):
-    filter = {"_id" : book_id}
+    filter = {"_id": book_id}
     book = _db.get_book(filter)
-    if not book: 
+    if not book:
         raise ApiBadRequest("Not found book")
     return json({
         'Book': book
     })
-
 
 
 @books_bp.route('/', methods={'POST'})
@@ -66,47 +61,46 @@ async def create_book(request, username=None):
     inserted = _db.add_book(book)
     if not inserted:
         raise ApiInternalError('Fail to create book')
-    
+
     async with request.app.ctx.redis as r:
         book_objs = _db.get_all_books()
         books = [book.to_dict() for book in book_objs]
         await set_cache(r, CacheConstants.all_books, books)
-    
+
     return json({'status': 'success'})
+
 
 @books_bp.route('/<book_id>', methods={'PUT'})
 @protected  # TODO: Authenticate
 @validate_with_jsonschema(update_book_json_schema)  # To validate request body
-
 async def update_book(request, book_id, username=None):
     filter_find = {"_id": book_id}
     book = _db.get_book(filter_find)
-    if not book : 
+    if not book:
         raise ApiNotFound('Dont exist book')
     if book['owner'] != username:
         raise ApiForbidden("Dont have permission to update this book")
-    
+
     filter_update = request.json
-    update = _db.update_book(filter_find,{"$set":filter_update})
-    
+    update = _db.update_book(filter_find, {"$set": filter_update})
+
     if not update:
         raise ApiInternalError('Fail to update book')
-    
+
     async with request.app.ctx.redis as r:
         book_objs = _db.get_all_books()
         books = [book.to_dict() for book in book_objs]
         await set_cache(r, CacheConstants.all_books, books)
-    
+
     return json({
         'status': "update success"
     })
-    
 @books_bp.route('/<book_id>', methods={'DELETE'})
 @protected  # TODO: Authenticate
 async def delete_book(request, book_id, username=None):
     filter_find = {"_id": book_id}
     book = _db.get_book(filter_find)
-    if not book : 
+    if not book:
         raise ApiNotFound('Dont exist book')
     if book['owner'] != username:
         raise ApiForbidden('Dont have permission to delete this book')
@@ -121,11 +115,3 @@ async def delete_book(request, book_id, username=None):
     return json({
         'status': "delete success"
     })
-    
-    
-
-    
-
-    
-
-
--- TrainingStudents-main/3.Backend/TrainingAPI/app/apis/user_blueprint.py
+++ TrainingStudents-main/3.Backend/TrainingAPI/app/apis/user_blueprint.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from sre_constants import SUCCESS
 import uuid
 import hashlib
 
@@ -7,43 +5,42 @@ from sanic import Blueprint
 from sanic.response import json
 from app.decorators.auth import protected
 from app.utils.jwt_utils import generate_jwt
-from app.constants.cache_constants import CacheConstants
 from app.databases.mongodb import MongoDB
-from app.databases.redis_cached import get_cache, set_cache
 from app.decorators.json_validator import validate_with_jsonschema
 from app.hooks.error import ApiBadRequest, ApiInternalError
 from app.models.user import user_json_schema, User
 from app.hooks.error import ApiInternalError
 
-
 users_bp = Blueprint('users_blueprint', url_prefix='/users')
 
 _db = MongoDB()
 
+
 # r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    
-@users_bp.route('/', methods = {'GET'})
+
+@users_bp.route('/', methods={'GET'})
 async def get_all_user(request):
     user_objs = _db.get_users()
-    users = [user.to_dict() for user in user_objs]        
+    users = [user.to_dict() for user in user_objs]
     number_of_users = len(users)
     return json({
         'n_users': number_of_users,
         'users': users
     })
-    
+
+
 @users_bp.route('/register', methods={'POST'})
 @validate_with_jsonschema(user_json_schema)  # To validate request body
 async def register_user(request):
     user_name = request.json["username"]
-    check_name = _db._users_col.find_one({"username":user_name})
+    check_name = _db._users_col.find_one({"username": user_name})
     if check_name:
         raise ApiBadRequest("Username existed")
-        
+
     user_id = str(uuid.uuid4())
     user_password = str(hashlib.md5(request.json["password"].encode()).hexdigest())
-    user = User(user_id,user_name,user_password)
-    register =  _db.register_user(user)
+    user = User(user_id, user_name, user_password)
+    register = _db.register_user(user)
     if not user:
         raise ApiInternalError('Fail to create user')
     # async with request.app.ctx.redis as r:
@@ -53,39 +50,36 @@ async def register_user(request):
     return json({
         'status': "User create success"
     })
-    
+
+
 @users_bp.route('/login', methods={'POST'})
 @validate_with_jsonschema(user_json_schema)
 async def login_user(request):
     user_name = request.json["username"]
     user_password = str(hashlib.md5(request.json["password"].encode()).hexdigest())
-    account = _db._users_col.find_one({"username" : user_name, "password" : user_password})
-    if not account :
+    account = _db._users_col.find_one({"username": user_name, "password": user_password})
+    if not account:
         raise ApiBadRequest("Wrong information")
-    
+
     token_jwt = generate_jwt(user_name).decode("utf-8")
-        
+
     return json({
         'status': "Login success",
-        'jwt_token' : token_jwt
+        'jwt_token': token_jwt
     })
-    
+
+
 @users_bp.route('/now', methods={'GET'})
 @protected
-async def now_user(request,username):
+async def now_user(request, username):
     return json({
-        'now user' : username
+        'now user': username
     })
-    
+
+
 @users_bp.route('/logout', methods={'GET'})
 @protected
-async def now_user(request,username):
+async def now_user(request, username):
     return json({
-        'status' : "logout success"
+        'status': "logout success"
     })
-
-    
-
-    
-
-
